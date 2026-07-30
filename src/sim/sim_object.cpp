bcfaf79189eb4183a153276cd2893aafb934b463
Pedroaliu <liuyu671026@gmail.com>
2026-07-30 14:53:28 +0800

    Polish SimObject lifecycle implementation

--- src/sim/sim_object.cpp
+++ src/sim/sim_object.cpp
@@ -1,7 +1,3 @@
-//
-// Created by pedroa on 2026/7/30.
-//
-
 #include "archlab/sim/sim_object.h"
 
 #include <stdexcept>
@@ -12,7 +8,7 @@ namespace archlab::sim {
 SimObject::SimObject(std::string name)
     : name_(std::move(name)) {
     if (name_.empty()) {
-        throw std::invalid_argument("name cannot be empty");
+        throw std::invalid_argument("SimObject name must not be empty");
     }
 }
 
@@ -26,26 +22,25 @@ LifecycleState SimObject::state() const {
 
 void SimObject::initialize() {
     if (state_ != LifecycleState::Constructed) {
-        throw std::logic_error(
-            name_ + ": initialize requires Constructed state");
+        throw std::logic_error(name_ + ": initialize requires Constructed state");
     }
+
     on_initialize();
     state_ = LifecycleState::Initialized;
 }
 
 void SimObject::reset() {
     if (state_ != LifecycleState::Initialized) {
-        throw std::logic_error(
-            name_ + ": reset requires Initialized state");
+        throw std::logic_error(name_ + ": reset requires Initialized state");
     }
+
     on_reset();
     state_ = LifecycleState::Ready;
 }
 
 void SimObject::startup() {
     if (state_ != LifecycleState::Ready) {
-        throw std::logic_error(
-            name_ + ": startup requires Ready state");
+        throw std::logic_error(name_ + ": startup requires Ready state");
     }
 
     on_startup();
@@ -53,23 +48,12 @@ void SimObject::startup() {
 }
 
 void SimObject::on_initialize() {
-
-}
-
-void SimObject::on_startup() {
-
-}
-
-void SimObject::on_shutdown() {
-
 }
 
 void SimObject::on_reset() {
-
 }
 
-
-
-
-
+void SimObject::on_startup() {
 }
+
+} // namespace archlab::sim
